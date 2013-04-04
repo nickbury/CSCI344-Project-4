5d1930280eb619d81aab13754f869347dcf1ab71
Nick Bury <nmbury@gmail.com>
2013-04-04 18:29:21 -0400

    Change forEach to regular ol' for

--- twitter.js
+++ twitter.js
@@ -1,13 +1,13 @@
 /*jslint indent:4*/
 
 var worker = function (trackedWords) {
-    var twitter = require("ntwitter");
-    var redis = require("redis");
-    var credentials = require("./credentials.js");
-                                                                                                                                                                                                                     
-    var client = redis.createClient();
+    var Twitter = require("ntwitter"),
+        redis = require("redis"),
+        credentials = require("./credentials.js"),
+        client = redis.createClient(),
+        i;
 
-    var t = new twitter({
+    var t = new Twitter({
         consumer_key: credentials.consumer_key,
         consumer_secret: credentials.consumer_secret,
         access_token_key: credentials.access_token_key,
@@ -19,11 +19,11 @@ var worker = function (trackedWords) {
         { track: trackedWords },
         function (stream) {
             stream.on("data", function (tweet) {
-                trackedWords.forEach(function (word) {
-                    if (tweet.text.indexOf(word) > -1) {
-                        client.incr(word);
+                for (i = 0; i < trackedWords.length; i++) {
+                    if (tweet.text.indexOf(trackedWords[i]) > -1) {
+                        client.incr(trackedWords[i]);
                     }
-                })
+                }
             });
         }
     );
