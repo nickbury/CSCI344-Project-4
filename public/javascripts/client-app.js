cf57bef64edf0947fcad81fb7a34b42162d7c1a8
Nick Bury <nmbury@gmail.com>
2013-04-04 21:40:01 -0400

    Add alert function to notify user which words are being used

--- public/javascripts/client-app.js
+++ public/javascripts/client-app.js
@@ -1,21 +1,25 @@
-/*jslint indent:4*/
-/*global $, document */
+/*jslint indent:4 plusplus:true*/
+/*global $, document, alert */
 
 var main = function () {
     var happyTotal = 0,
         sadTotal = 0,
         happyWidth,
-        sadWidth;
+        sadWidth,
+        happyWords = [],
+        sadWords = [];
 
     $.getJSON("/happyCounts.json", function (response) {
         response.forEach(function (result) {
             happyTotal = happyTotal + JSON.parse(result.value);
+            happyWords.push(" " + result.key);
         });
     });
 
     $.getJSON("/sadCounts.json", function (response) {
         response.forEach(function (result) {
             sadTotal = sadTotal + JSON.parse(result.value);
+            sadWords.push(" " + result.key);
             happyWidth = (happyTotal / (happyTotal + sadTotal)) * 100;
             sadWidth = 100 - happyWidth;
             $(".happy").width(happyWidth + '%');
@@ -23,6 +27,14 @@ var main = function () {
             $("#happyCount").html(happyTotal);
             $("#sadCount").html(sadTotal);
         });
+    });
+
+    $(".happy").click(function (event) {
+        alert(happyWords);
+    });
+
+    $(".sad").click(function (event) {
+        alert(sadWords);
     });
 
 };
