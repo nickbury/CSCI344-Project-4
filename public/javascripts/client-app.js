effd94d53fa14d615f903086f706a54a475a8a34
Nick Bury <nmbury@gmail.com>
2013-04-04 21:03:33 -0400

    Total happy and sad counts and display as a percentage of width

--- public/javascripts/client-app.js
+++ public/javascripts/client-app.js
@@ -1,7 +1,30 @@
+/*jslint indent:4*/
+/*global $, document */
+
 var main = function () {
-    $.getJSON("/counts.json", function (response) {
-        $("body").append("<p>awesome:"+response.awesome+"</p>");
+    var happyTotal = 0,
+        sadTotal = 0,
+        happyWidth,
+        sadWidth;
+
+    $.getJSON("/happyCounts.json", function (response) {
+        response.forEach(function (result) {
+            happyTotal = happyTotal + JSON.parse(result.value);
+        });
     });
+
+    $.getJSON("/sadCounts.json", function (response) {
+        response.forEach(function (result) {
+            sadTotal = sadTotal + JSON.parse(result.value);
+            happyWidth = (happyTotal / (happyTotal + sadTotal)) * 100;
+            sadWidth = 100 - happyWidth;
+            $(".happy").width(happyWidth + '%');
+            $(".sad").width(sadWidth + '%');
+            $("#happyCount").html(happyTotal);
+            $("#sadCount").html(sadTotal);
+        });
+    });
+
 };
 
 $(document).ready(main);
